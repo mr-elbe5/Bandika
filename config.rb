88cfea7f6ae329976959f8cfed26af63ae0b0940
miro-e25 <mroennau@elbdeich25.de>
2017-06-17 11:37:29 +0200

    scss with compass done

--- config.rb
+++ config.rb
@@ -3,18 +3,22 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-add_import_path "webbase/web/_statics/scss"
+add_import_path "webbase/scss"
 css_dir = "cms/web/_statics/css"
-sass_dir = "cms/web/_statics/scss"
-images_dir = "cms/web/_statics/css/img"
+sass_dir = "cms/scss"
+images_dir = "cms/web/_statics/img"
+http_images_path = "/_statics/img"
+fonts_dir = "cms/web/_statics/fonts"
+http_fonts_path = "/_statics/fonts"
 javascripts_dir = "cms/web/_statics/js"
 sourcemap = true
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :compressed
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
+#relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
