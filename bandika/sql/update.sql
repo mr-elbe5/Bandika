56787dd9fe361b369ef1aea0d23c4cbfebdde499
Michael Rönnau <mroennau@elbdeich25.de>
2020-04-11 11:59:24 +0200

    fix

--- bandika/sql/update.sql
+++ bandika/sql/update.sql
@@ -1,1 +1,15 @@
--- version 11.0
+drop table t_editor_page;
+
+-- salt is V3xfgDrxdl8=
+-- root user
+update t_user set pwd='A0y3+ZmqpMhWA21VFQMkyY6v74Y=' where id=1;
+
+-- root page
+SELECT ADDSECTIONPAGE(1, null, 'home', 'Home', 'Startseite', 1, 'defaultSectionPage');
+UPDATE t_page set publish_date=now() where id =1;
+
+
+
+
+
+--local ok
