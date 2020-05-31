26864a01704740b5ee23ce11fb8687f6796fa84e
Michael Rönnau <mroennau@elbdeich25.de>
2020-05-31 12:56:19 +0200

    remote server

--- bandika-app/sql/update.sql
+++ bandika-app/sql/update.sql
@@ -1,5 +1,3 @@
-drop table t_editor_page;
-
 -- salt is V3xfgDrxdl8=
 -- root user
 update t_user set pwd='A0y3+ZmqpMhWA21VFQMkyY6v74Y=' where id=1;
@@ -7,9 +5,6 @@ update t_user set pwd='A0y3+ZmqpMhWA21VFQMkyY6v74Y=' where id=1;
 -- root page
 SELECT ADDSECTIONPAGE(1, null, 'home', 'Home', 'Startseite', 1, 'defaultSectionPage');
 UPDATE t_page set publish_date=now() where id =1;
-
-
-
 
 
 --local ok
