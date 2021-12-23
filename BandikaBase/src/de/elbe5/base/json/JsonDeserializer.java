a5e7019abd83c55cc057bc7771efe2725326804f
mr-elbe5 <miro@elbe5.de>
2021-12-23 11:26:26 +0100

    removed jjwt

--- BandikaBase/src/de/elbe5/base/json/JsonDeserializer.java
+++ BandikaBase/src/de/elbe5/base/json/JsonDeserializer.java
@@ -1,47 +1,43 @@
 package de.elbe5.base.json;
 
-import io.jsonwebtoken.io.DeserializationException;
-import io.jsonwebtoken.io.Deserializer;
-import io.jsonwebtoken.lang.Assert;
-import io.jsonwebtoken.lang.Strings;
 import org.json.simple.parser.JSONParser;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
-public class JsonDeserializer implements Deserializer {
+    public class JsonDeserializer {
 
-    public Object deserialize(InputStream in) throws DeserializationException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        byte[] buffer = new byte[0x4000];
-        int len;
-        try {
-            while ((len = in.read(buffer, 0, 0x4000)) > 0) {
-                outputStream.write(buffer, 0, len);
+        public Object deserialize(InputStream in) throws IOException {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            byte[] buffer = new byte[0x4000];
+            int len;
+            try {
+                while ((len = in.read(buffer, 0, 0x4000)) > 0) {
+                    outputStream.write(buffer, 0, len);
+                }
+            } catch (IOException e) {
+                throw new IOException(e.getMessage());
             }
-        } catch (IOException e){
-            throw new DeserializationException(e.getMessage());
+            return deserialize(outputStream.toByteArray());
         }
-        return deserialize(outputStream.toByteArray());
+
+        public Object deserialize(byte[] bytes) throws IOException {
+
+            if (bytes == null) {
+                throw new IOException("JSON byte array cannot be null");
+            }
+            if (bytes.length == 0) {
+                throw new IOException("Invalid JSON: zero length byte array.");
+            }
+            try {
+                String s = new String(bytes, StandardCharsets.UTF_8);
+                return new JSONParser().parse(s);
+            } catch (Exception e) {
+                String msg = "Invalid JSON: " + e.getMessage();
+                throw new IOException(msg, e);
+            }
+        }
+
     }
-
-    @Override
-    public Object deserialize(byte[] bytes) throws DeserializationException {
-
-        Assert.notNull(bytes, "JSON byte array cannot be null");
-
-        if (bytes.length == 0) {
-            throw new DeserializationException("Invalid JSON: zero length byte array.");
-        }
-
-        try {
-            String s = new String(bytes, Strings.UTF_8);
-            return new JSONParser().parse(s);
-        } catch (Exception e) {
-            String msg = "Invalid JSON: " + e.getMessage();
-            throw new DeserializationException(msg, e);
-        }
-    }
-
-}
