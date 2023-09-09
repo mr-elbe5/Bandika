2622a29c7306a2504d51d14dc62dc8d09aa3bb50
miro <miro@elbe5.de>
2023-09-09 10:18:35 +0200

    sql update

--- sql/init.sql
+++ sql/init.sql
@@ -1,3 +1,6 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
+
 CREATE SEQUENCE s_group_id START 1000;
 CREATE TABLE IF NOT EXISTS t_group
 (
@@ -121,10 +124,95 @@ CREATE OR REPLACE VIEW v_preview_file as (
                                          where t_file.id=t_image.id
                                              );
 
+CREATE TABLE IF NOT EXISTS t_extended_user
+(
+    id                 INTEGER      NOT NULL,
+    first_name         VARCHAR(100) NOT NULL DEFAULT '',
+    street             VARCHAR(100) NOT NULL DEFAULT '',
+    zipCode            VARCHAR(16)  NOT NULL DEFAULT '',
+    city               VARCHAR(50)  NOT NULL DEFAULT '',
+    country            VARCHAR(50)  NOT NULL DEFAULT '',
+    phone              VARCHAR(50)  NOT NULL DEFAULT '',
+    mobile             VARCHAR(50)  NOT NULL DEFAULT '',
+    notes              VARCHAR(500) NOT NULL DEFAULT '',
+    CONSTRAINT t_extended_user_pk PRIMARY KEY (id),
+    CONSTRAINT t_extended_user_fk1 FOREIGN KEY (id) REFERENCES t_user(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS t_page
+(
+    id            INTEGER       NOT NULL,
+    keywords      VARCHAR(500)  NOT NULL DEFAULT '',
+    layout        VARCHAR(255) NOT NULL default 'defaultPage',
+    publish_date  TIMESTAMP     NULL,
+    published_content TEXT      NOT NULL DEFAULT '',
+    CONSTRAINT t_page_pk PRIMARY KEY (id),
+    CONSTRAINT t_page_fk1 FOREIGN KEY (id) REFERENCES t_content (id) ON DELETE CASCADE
+);
+
+CREATE OR REPLACE FUNCTION ADDPAGE (id INTEGER,parent_id INTEGER,name VARCHAR,display_name VARCHAR,
+                                    description VARCHAR,user_id INTEGER, layout VARCHAR)
+    RETURNS VOID AS
+$$
+BEGIN
+    INSERT INTO t_content (id,type,parent_id,ranking,name,display_name,description,creator_id,changer_id,open_access,nav_type)
+    VALUES (id,'de.elbe5.page.PageData',parent_id,0,name,display_name,description,user_id,user_id,true,'HEADER');
+    INSERT INTO t_page (id, keywords, layout)
+    VALUES (id, '', layout);
+END
+$$
+    LANGUAGE plpgsql;
+
+CREATE TABLE IF NOT EXISTS t_content_log
+(
+    content_id INTEGER     NOT NULL,
+    day        DATE        NOT NULL,
+    count      INTEGER 	   NOT NULL,
+    CONSTRAINT t_content_log_pk PRIMARY KEY (content_id, day),
+    CONSTRAINT t_content_log_fk1 FOREIGN KEY (content_id) REFERENCES t_content (id) ON DELETE CASCADE
+);
+
+CREATE SEQUENCE s_page_part_id START 1000;
+
+CREATE TABLE IF NOT EXISTS t_page_part
+(
+    id            INTEGER      NOT NULL,
+    type          VARCHAR(30)  NOT NULL DEFAULT 'LayoutPartData',
+    page_id       INTEGER      NULL,
+    section       VARCHAR(60)  NOT NULL,
+    position      INTEGER      NOT NULL DEFAULT 0,
+    name          VARCHAR(60)  NOT NULL DEFAULT '',
+    creation_date TIMESTAMP    NOT NULL DEFAULT now(),
+    change_date   TIMESTAMP    NOT NULL DEFAULT now(),
+    CONSTRAINT t_page_part_pk PRIMARY KEY (id),
+    CONSTRAINT t_page_part_fk1 FOREIGN KEY (page_id) REFERENCES t_page (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS t_layout_part
+(
+    id            INTEGER      NOT NULL,
+    layout        VARCHAR(255) NOT NULL,
+    CONSTRAINT t_layout_part_pk PRIMARY KEY (id),
+    CONSTRAINT t_layout_part_fk1 FOREIGN KEY (id) REFERENCES t_page_part (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS t_part_field
+(
+    part_id    INTEGER     NOT NULL,
+    field_type VARCHAR(60) NOT NULL,
+    name       VARCHAR(60) NOT NULL DEFAULT '',
+    content    TEXT        NOT NULL DEFAULT '',
+    CONSTRAINT t_part_field_pk PRIMARY KEY (part_id, name),
+    CONSTRAINT t_part_field_fk1 FOREIGN KEY (part_id) REFERENCES t_page_part (id) ON DELETE CASCADE
+);
 
 -- root user
 INSERT INTO t_user (id,type,name,email,login,pwd)
-VALUES (1,'de.elbe5.user.UserData','Administrator','root@localhost','root','');
+VALUES (1,'de.elbe5.extendeduser.ExtendedUserData','Administrator','root@localhost','root','');
+
+-- root
+SELECT ADDPAGE(1, null, 'home', 'Home', 'Home Page', 1, 'defaultPage');
+
 
 -- timer
 INSERT INTO t_timer_task (name,display_name,execution_interval,minute,active)
